9f70fb2102b93e1066a1d6dade1a7e8b0a0d81cf
Beyzanur <oztasbeyzanur@gmail.com>
2021-03-14 22:50:58 +0300

    Add project files.

---
+++ MyDictionary/Dictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class Dictionary<TKey,TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        public Dictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey ID,TValue Book)
+        {
+            TKey[] tempKey = keys;
+            TValue[] tempValue = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+                values[i] = tempValue[i];
+            }
+
+            keys[keys.Length - 1] = ID;
+            values[values.Length - 1] = Book;
+        }
+
+        public int Length { get { return keys.Length; } }
+        public TValue[] Items { get { return values; } }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(1, "Satranç");
+            dictionary.Add(2, "Bülbülü Öldürmek");
+            dictionary.Add(3, "Dönüşüm");
+            dictionary.Add(4, "Yaşamak");
+           
+            for (int i = 0; i < dictionary.Items.Length; i++)
+            {
+                Console.WriteLine(dictionary.Items[i]);
+            }
+
+            Console.WriteLine(dictionary.Length);
+            dictionary.Add(5, "Simyacı");
+            Console.WriteLine(dictionary.Length +". nolu ID'ye eklenen kitap: "+dictionary.Items[dictionary.Length-1]);
+        }
+    }
+}
